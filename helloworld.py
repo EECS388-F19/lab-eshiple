85b8ebe551e1bebda07d1f1c4da16e8ac58d9a4e
Eric <ericshiple@gmail.com>
2019-09-09 17:40:48 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Eric")
+first = random.randint(0,101)
+print(first)
+second = random.randint(0,101)
+print(second)
+total = first + second
+print("Sum = " + str(total))
+print("Average = " + str(total / 2))
+
